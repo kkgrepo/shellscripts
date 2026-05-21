a1895e35998d72afb494846ff3bacaa9950b20e0
Deeptha Gudla <kiranncr@gmail.com>
2026-05-21 20:41:59 +0530

    adding 3rd file

---
+++ third.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+echo "executed at"
+echo (date)
+
+echo "enter name ..."
+read USER_NAME
+
+echo "enter password ..."
+read -s PASS_WORD
+
+echo "uname - $USER_NAME #### password is $PASS_WORD"
+
+echo "thank you"
